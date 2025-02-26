26e8a1dad45efe44c8dfeac95d6232ec16ffa53c
logansw <logansw428@gmail.com>
2025-02-26 15:57:09 -0800

    Game settings set up

---
+++ Assets/_Project/Scripts/GameSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct GameSettings
+{
+    public float Time;
+
+    public GameSettings(float time)
+    {
+        Time = time;
+    }
+}
