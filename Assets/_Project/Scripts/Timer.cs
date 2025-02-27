e3d8cc074097b67ad8a18e196d151ff5dda50883
logansw <logansw428@gmail.com>
2025-02-26 16:10:17 -0800

    [Feat]: Add timer

--- Assets/_Project/Scripts/Timer.cs
+++ Assets/_Project/Scripts/Timer.cs
@@ -1,8 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Timer : MonoBehaviour
 {
+    private float _secondsRemaining;
+    [SerializeField] private TMP_Text _text;
+    public static Action OnTimerComplete;
+    private Coroutine _timerCoroutine;
+
+    void Start()
+    {
+        StartTimer(60);
+    }
+
+    public void StartTimer(float startTime)
+    {
+        _secondsRemaining = startTime;
+        _timerCoroutine = StartCoroutine(UpdateTimer());
+    }
     
+    public IEnumerator UpdateTimer()
+    {
+        while (_secondsRemaining > 0)
+        {
+            _secondsRemaining -= Time.deltaTime;
+            int minutes = (int)(_secondsRemaining / 60);
+            int seconds = (int)(_secondsRemaining % 60);
+            _text.text = $"{minutes}:{seconds}";
+            yield return null;
+        }
+
+        OnTimerComplete?.Invoke();
+    }
 }
